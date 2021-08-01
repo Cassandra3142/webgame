570c7789369277dd167ce1309940ea6f54a68df9
Cassandra3142 <85780707+Cassandra3142@users.noreply.github.com>
2021-08-01 17:02:46 +0900

    이벤트 리스너 달기

--- word-relay/script.js
+++ word-relay/script.js
@@ -3,3 +3,16 @@ const number = parseInt(prompt('몇 명이 참가하나요?'), 10);
 alert(number);
 
 const yesOrNo = confirm('맞나요?');
+const $Input = document.querySelector('input');
+const $Button = document.querySelector('button');
+
+function wording () {
+    console.log(event.target.value);
+}
+
+function onClick () {
+    console.log('버튼클릭');
+}
+
+$Input.addEventListener('input',wording);
+$Button.addEventListener('click',onClick);
