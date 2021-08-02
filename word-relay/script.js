e3a2c3346510538d841be59dbe7ad77e0c3658fe
Cassandra3142 <85780707+Cassandra3142@users.noreply.github.com>
2021-08-02 14:53:12 +0900

    word-relay : 첫 번째 사람인지 판별하기

--- word-relay/script.js
+++ word-relay/script.js
@@ -5,14 +5,23 @@ alert(number);
 const yesOrNo = confirm('맞나요?');
 const $Input = document.querySelector('input');
 const $Button = document.querySelector('button');
+const $word = document.querySelector('#word');
+let word ; // 제시어
+let newWord ; // 새로 입력 단어
 
-function wording () {
-    console.log(event.target.value);
+function onInput () {
+    newWord = event.target.value;
 }
 
-function onClick () {
-    console.log('버튼클릭');
+function onClickButton () { // 제시어가 비어 있는가?
+    if (!word) { // 제시어가 비어있는 경우
+        word = newWord
+        $word.textContent = word;
+        $Input.value = ' '
+    } else { // 제시어가 있는 경우
+
+    }
 }
 
-$Input.addEventListener('input',wording);
-$Button.addEventListener('click',onClick);
+$Input.addEventListener('input',onInput);
+$Button.addEventListener('click',onClickButton);
