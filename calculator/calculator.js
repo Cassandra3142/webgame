87518ecc63ad06fcc7b64c6e802714ce1484f98b
Cassandra3142 <85780707+Cassandra3142@users.noreply.github.com>
2021-08-04 16:06:08 +0900

    calculator : 함수 중복 제거 (고차 함수)

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -4,16 +4,25 @@ let numTwo = '';
 const $operator = document.querySelector('#operator');
 const $result = document.querySelector('#result');
 
-document.querySelector('#num-0').addEventListener('click', () => {});
-document.querySelector('#num-1').addEventListener('click', () => {});
-document.querySelector('#num-2').addEventListener('click', () => {});
-document.querySelector('#num-3').addEventListener('click', () => {});
-document.querySelector('#num-4').addEventListener('click', () => {});
-document.querySelector('#num-5').addEventListener('click', () => {});
-document.querySelector('#num-6').addEventListener('click', () => {});
-document.querySelector('#num-7').addEventListener('click', () => {});
-document.querySelector('#num-8').addEventListener('click', () => {});
-document.querySelector('#num-9').addEventListener('click', () => {});
+const onClickNumber = (number) => () => {
+    if(!operator) { //operator가 비어있다.
+        numOne += number;
+    }else { // operator가 있다.
+        numTwo += number;
+    }
+    $result.value += number;
+}; // 고차 함수 (high order function)
+
+document.querySelector('#num-0').addEventListener('click',onClickNumber('0'));
+document.querySelector('#num-1').addEventListener('click',onClickNumber('1'));
+document.querySelector('#num-2').addEventListener('click',onClickNumber('2'));
+document.querySelector('#num-3').addEventListener('click',onClickNumber('3'));
+document.querySelector('#num-4').addEventListener('click',onClickNumber('4'));
+document.querySelector('#num-5').addEventListener('click',onClickNumber('5'));
+document.querySelector('#num-6').addEventListener('click',onClickNumber('6'));
+document.querySelector('#num-7').addEventListener('click',onClickNumber('7'));
+document.querySelector('#num-8').addEventListener('click',onClickNumber('8'));
+document.querySelector('#num-9').addEventListener('click',onClickNumber('9'));
 document.querySelector('#plus').addEventListener('click', () => {});
 document.querySelector('#minus').addEventListener('click', () => {});
 document.querySelector('#divide').addEventListener('click', () => {});
