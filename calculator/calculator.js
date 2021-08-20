59cc1ecd7b80cce2d046d46ffcb6f2cb5ce32a78
kimseongtae <rlatjdxo4907@naver.com>
2021-08-20 17:15:19 +0900

    함수 중복 제거 (고차함수) + operator추가

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -4,28 +4,37 @@ let numTwo = '';
 const $operator = document.querySelector('#operator');
 const $result = document.querySelector('#result');
 
-const onClickNumber = (number) => () => {
-    if(!operator) { //operator가 비어있다.
-        numOne += number;
-    }else { // operator가 있다.
-        numTwo += number;
+const onClickNumber = (event) => {
+    if(!operator) {
+        numOne += event.target.textContent;
+    } else {
+        numTwo += event.target.textContent;
     }
-    $result.value += number;
-}; // 고차 함수 (high order function)
+    $result.value += event.target.textContent;
+};
 
-document.querySelector('#num-0').addEventListener('click',onClickNumber('0'));
-document.querySelector('#num-1').addEventListener('click',onClickNumber('1'));
-document.querySelector('#num-2').addEventListener('click',onClickNumber('2'));
-document.querySelector('#num-3').addEventListener('click',onClickNumber('3'));
-document.querySelector('#num-4').addEventListener('click',onClickNumber('4'));
-document.querySelector('#num-5').addEventListener('click',onClickNumber('5'));
-document.querySelector('#num-6').addEventListener('click',onClickNumber('6'));
-document.querySelector('#num-7').addEventListener('click',onClickNumber('7'));
-document.querySelector('#num-8').addEventListener('click',onClickNumber('8'));
-document.querySelector('#num-9').addEventListener('click',onClickNumber('9'));
-document.querySelector('#plus').addEventListener('click', () => {});
-document.querySelector('#minus').addEventListener('click', () => {});
-document.querySelector('#divide').addEventListener('click', () => {});
-document.querySelector('#multiply').addEventListener('click', () => {});
-document.querySelector('#calculate').addEventListener('click', () => {});
-document.querySelector('#clear').addEventListener('click', () => {});
+const onClickOperator = (event) => {
+    if(numOne) {
+        operator = event.target.textContent;
+        $operator.value =event.target.textContent;
+    } else {
+        alert('첫번째 숫자를 먼저 입력하세요');
+    }
+}
+
+document.querySelector('#num-0').addEventListener('click',onClickNumber);
+document.querySelector('#num-1').addEventListener('click',onClickNumber);
+document.querySelector('#num-2').addEventListener('click',onClickNumber);
+document.querySelector('#num-3').addEventListener('click',onClickNumber);
+document.querySelector('#num-4').addEventListener('click',onClickNumber);
+document.querySelector('#num-5').addEventListener('click',onClickNumber);
+document.querySelector('#num-6').addEventListener('click',onClickNumber);
+document.querySelector('#num-7').addEventListener('click',onClickNumber);
+document.querySelector('#num-8').addEventListener('click',onClickNumber);
+document.querySelector('#num-9').addEventListener('click',onClickNumber);
+document.querySelector('#plus').addEventListener('click', onClickOperator);
+document.querySelector('#minus').addEventListener('click', onClickOperator);
+document.querySelector('#divide').addEventListener('click', onClickOperator);
+document.querySelector('#multiply').addEventListener('click', onClickOperator);
+document.querySelector('#calculate').addEventListener('click', onClickOperator);
+document.querySelector('#clear').addEventListener('click', onClickOperator);
