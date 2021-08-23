e90153dcfb680e550ad4b2a5cea08870a51053c3
kimseongtae <rlatjdxo4907@naver.com>
2021-08-23 21:42:36 +0900

    num-baseball : 무작위로 공 선택하기

--- num-baseball/num-baseball.js
+++ num-baseball/num-baseball.js
@@ -1,3 +1,17 @@
 const $form = document.querySelector('#form');
 const $input = document.querySelector('#input');
 const $logs = document.querySelector('#logs');
+
+const numbers = []; //1번 ~ 9번 공
+for (let n = 0; n < 9; n++) {
+  numbers.push(n + 1);
+}
+
+const answer = []; //무작위로 공 네개 선택
+for (let n = 0; n < 4; n++) {
+  const index = Math.floor(Math.random() * numbers.length);
+  answer.push(numbers[index]);
+  numbers.slice(index,1);
+}
+
+console.log(answer);
