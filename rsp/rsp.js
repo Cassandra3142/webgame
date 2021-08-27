fcd33b6d7a7238f4368a3b671f2edcf2f24e04f6
kimseongtae <rlatjdxo4907@naver.com>
2021-08-27 13:37:29 +0900

    rsp : 초기셋팅 + 객체

---
+++ rsp/rsp.js
@@ -0,0 +1,18 @@
+const $computer = document.querySelector('#computer');
+const $score = document.querySelector('#score');
+const $rock = document.querySelector('#rock');
+const $scissors = document.querySelector('#scissors');
+const $paper = document.querySelector('#paper');
+const IMG_URL = './rsp.png';
+$computer.style.background = `url(${IMG_URL}) -464px 0`;
+$computer.style.backgroundSize = 'auto 200px';
+
+// const scissorsX = '0px';
+// const rockX = '-220px';
+// const paperX = '440px';
+
+const rspX = { // 위의 변수들을 객체로 묶어서 표현
+  scissors : '0px',
+  rock : '-220px',
+  paper : '-440px',
+}
