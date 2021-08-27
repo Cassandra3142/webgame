91eb975ada64f49017dede5718fd8f88c1be1f73
kimseongtae <rlatjdxo4907@naver.com>
2021-08-27 16:04:40 +0900

    rsp : 타이머 멈췄다가 다시 실행하기

--- rsp/rsp.js
+++ rsp/rsp.js
@@ -18,17 +18,34 @@ const rspX = { // 위의 변수들을 객체로 묶어서 표현
 }
 
 let computerChoice = 'scissors';
+
 const changeComputerChoice = () => {
-  if(computerChoice === 'rock') {
-    computerChoice = 'scissors';
-  } else if (computerChoice === 'scissors') {
-    computerChoice = 'paper';
-  } else if (computerChoice ==='paper') {
-    computerChoice = 'rock';
+  if(computerChoice === 'rock') { // 바위 일때
+    computerChoice = 'scissors'; // 가위로 바꿔 줌
+  } else if (computerChoice === 'scissors') { // 가위 일때
+    computerChoice = 'paper'; // 보로 바꿔 줌
+  } else if (computerChoice ==='paper') { // 보 일때
+    computerChoice = 'rock'; // 주먹으로 바꿔 줌
   }
   $computer.style.background = `url(${IMG_URL}) ${rspX[computerChoice]} 0`;
   $computer.style.backgroundSize = 'auto 200px'
-  setTimeout(changeComputerChoice, 50);
 }
 
-setTimeout(changeComputerChoice, 50);
+let intervalId = setInterval(changeComputerChoice,50);
+
+let clickable = true;
+const clickBtn = () => {
+  if (clickable) {
+    clearInterval(intervalId);
+    clickable = false;
+    // 점수 계산 및 화면 표시
+    setTimeout(() =>{
+      clickable = true;
+      intervalId = setInterval(changeComputerChoice,50);
+    },1000);
+  }
+};
+
+$rock.addEventListener('click', clickBtn);
+$scissors.addEventListener('click', clickBtn);
+$paper.addEventListener('click', clickBtn);
