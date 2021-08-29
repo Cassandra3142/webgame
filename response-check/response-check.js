dfcbe8a91ef8a94b7980f18526a04b4417010011
kimseongtae <rlatjdxo4907@naver.com>
2021-08-29 13:55:59 +0900

    response-check : 성급한 클릭 막기

--- response-check/response-check.js
+++ response-check/response-check.js
@@ -4,22 +4,24 @@ const $result = document.querySelector('#result');
 let startTime;
 let endTime;
 const records = [];
+let timeoutId;
+
 $screen.addEventListener('click',(event) => {
   if(event.target.classList.contains('waiting')) {
     $screen.classList.remove('waiting');
     $screen.classList.add('ready');
     $screen.textContent = '초록색이 되면 클릭하세요';
-    setTimeout(function() {
+    timeoutId = setTimeout(function() {
       startTime = new Date();
       $screen.classList.remove('ready');
       $screen.classList.add('now');
       $screen.textContent = '클릭하세요'
     }, Math.floor(Math.random() * 1000) + 2000);
   } else if (event.target.classList.contains('ready')) {
+    clearTimeout(timeoutId);
     $screen.classList.remove('ready');
     $screen.classList.add('waiting');
-    $screen.textContent = '클릭해서 시작하세요'
-    alert('성급했습니다.')
+    $screen.textContent = '성급하셨군요. 빨간색에서는 클릭을 하지마세요';
   } else if (event.target.classList.contains('now')) {
     endTime = new Date();
     const current = endTime - startTime;
