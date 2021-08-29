1cff4577ee9f095e0a4d461cad0dfb6aca876dc1
kimseongtae <rlatjdxo4907@naver.com>
2021-08-29 20:41:29 +0900

    tictactoe : 표 직접 그리기

--- tictactoe/tictactoe.js
+++ tictactoe/tictactoe.js
@@ -1,16 +1,48 @@
-const data = [];
-for (let i = 0 ; i < 3 ; i ++) {
-  data.push([]);
-}
+// const data = [];
+// for (let i = 0 ; i < 3 ; i ++) {
+//   data.push([]);
+// }
 
+// let turn = 'O';
+
+// const $table = document.createElement('table');
+// document.body.append($table);
+// for (let i = 0 ; i < 3 ; i ++) {
+//   const $tr = document.createElement('tr');
+//   for (let i = 0 ; i < 3 ; i ++) {
+//     const $td = document.createElement('td');
+//     $td.addEventListener('click', (event) => {
+//       if(event.target.textContent) {
+//         return;
+//       }
+//       event.target.textContent = turn
+//       if(turn === 'O') {
+//         turn = 'X';
+//       } else if (turn === 'X') {
+//         turn = 'O';
+//       }
+//     })
+//     $tr.append($td);
+//   }
+//   $table.append($tr);
+// }
+
+const { body } = document;
 const $table = document.createElement('table');
-document.body.append($table);
-for (let i = 0 ; i < 3 ; i ++) {
+const $result = document.createElement('div');
+const rows = [];
+
+for (let i = 0; i < 3; i++) {
   const $tr = document.createElement('tr');
-  for (let i = 0 ; i < 3 ; i ++) {
+  const cells = [];
+  for (let j = 0; j < 3; j++) {
     const $td = document.createElement('td');
-    $tr.append($td);
+    cells.push($td);
+    $tr.appendChild($td);
   }
-  $table.append($tr);
+  rows.push(cells);
+  $table.appendChild($tr);
 }
 
+body.appendChild($table);
+body.appendChild($result);
