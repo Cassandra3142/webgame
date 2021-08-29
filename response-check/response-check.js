de5a428e431f384f8be66cc5edc731236de3daaf
kimseongtae <rlatjdxo4907@naver.com>
2021-08-29 11:24:11 +0900

    response-check : 반응속도 측정하기

--- response-check/response-check.js
+++ response-check/response-check.js
@@ -1,12 +1,16 @@
 const $screen = document.querySelector('#screen');
 const $result = document.querySelector('#result');
 
+let startTime;
+let endTime;
+
 $screen.addEventListener('click',(event) => {
   if(event.target.classList.contains('waiting')) {
     $screen.classList.remove('waiting');
     $screen.classList.add('ready');
     $screen.textContent = '초록색이 되면 클릭하세요';
     setTimeout(function() {
+      startTime = new Date();
       $screen.classList.remove('ready');
       $screen.classList.add('now');
       $screen.textContent = '클릭하세요'
@@ -17,6 +21,10 @@ $screen.addEventListener('click',(event) => {
     $screen.textContent = '클릭해서 시작하세요'
     alert('성급했습니다.')
   } else if (event.target.classList.contains('now')) {
-
+    endTime = new Date();
+    $result.textContent = `${endTime - startTime} ms`;
+    $screen.classList.remove('now');
+    $screen.classList.add('waiting');
+    $screen.textContent = '클릭해서 시작하세요'
   }
 })
